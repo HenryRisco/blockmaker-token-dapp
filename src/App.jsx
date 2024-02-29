080239e4b2ea9cc3ef0057921ab3133bda6025a2
HenryRisco <lhrisco@gmail.com>
2024-02-29 13:31:59 +0100

    Actualizar App y añadir configuracion de wagmi

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
+import { ConnectKitProvider } from 'connectkit'
 import { AppLayout } from './components/ui/layouts'
 import { Home } from './pages'
+import { WagmiConfig } from 'wagmi'
+import { config } from './config/wagmi'
+
+console.log('alchemy ID', import.meta.env.VITE_ALCHEMY_ID)
+console.log('wallet connect ID', import.meta.env.VITE_WALLETCONNECT_PROJECT_ID)
+
 function App() {  
   return (
-    <AppLayout>
-      <Home />
-    </AppLayout>
+    <WagmiConfig config={config}>
+      <ConnectKitProvider mode='light'>
+        <AppLayout>
+          <Home />
+        </AppLayout>
+      </ConnectKitProvider>
+    </WagmiConfig>
+    
   )
 }
 
