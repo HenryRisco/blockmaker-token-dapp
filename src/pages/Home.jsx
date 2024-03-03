c93438849d99c47a8828a47d97e32892a2cebd0a
HenryRisco <lhrisco@gmail.com>
2024-03-03 19:13:46 +0100

    Actualizar componente Home: - Implementar hook useAccount - Añadir componentes de ui de muestra

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,28 @@
+import { useAccount } from "wagmi"
+import { LoadingSpinner, Button, ErrorInfo, TextInput, Title } from "../components/ui"
+import { Owner } from "../components"
+
 export default function Home() {
+    const { address, isConnecting, isDisconnected } = useAccount()
+
+    if (isConnecting) return <div>Connecting...</div>
+    if (isDisconnected) return <div>Disconnected</div>
+    
     return (
-        <div>
-            <h1 className="text-3xl font-bold underline">Hello World!</h1>
+        <div className="flex flex-col gap-6"> 
+            <div>Connected Wallet: {address}</div>
+            <LoadingSpinner className="h-12 w-12" />
+            <div>
+                <Button>Comprar Tokens</Button>
+            </div>
+            <ErrorInfo message="Error: Internal Server Error"/>
+            <div>
+                <TextInput />
+            </div>
+            <Title>Este es un titulo de muestra</Title> 
+            <Owner />
         </div>
+    
+    
     )
 }
